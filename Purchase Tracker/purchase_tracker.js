a726038f2f7d6023baac358f67b6f1c2d187a508
Ruthu Mavnur <ru2.mavnur@gmail.com>
2023-02-05 12:21:48 -0800

    the same thing as before (purchase tracker)

--- Purchase Tracker/purchase_tracker.js
+++ Purchase Tracker/purchase_tracker.js
@@ -1,4 +1,5 @@
 function newPurchase() {
+    /*
     var date = document.getElementById("dateInput");
     var money = document.getElementById("moneyInput");
     var title = document.getElementById("titleInput");
@@ -18,9 +19,50 @@ function newPurchase() {
     document.getElementById("dateList").appendChild(dateLI);
     document.getElementById("titleList").appendChild(titleLI);
     document.getElementById("moneyList").appendChild(moneyLI);
+    */
+
+    var date = document.getElementById("dateInput");
+    var money = document.getElementById("moneyInput");
+    var title = document.getElementById("titleInput");
+
+    var purchaseLI = document.createElement("li");
+    
+    purchaseLI.innerHTML = date.value + " , \"" + title.value + "\", $" + money.value;
+    date.value = "";
+    title.value = "";
+    money.value = "";
+    document.getElementById("purchaseList").appendChild(purchaseLI);
 }
 
 function removePurchase() {
    var memo = getElementById("memo");
    memo.moneyList.removeChild(memo);
 }
+
+function createCloseButton() {
+    // Create a "close" button and append it to each list item
+    var crossList = document.getElementsByTagName("li");
+    var i;
+    for (i = 0; i < crossList.length; i++) {
+        var spanVar = document.createElement("span");
+        var txt = document.createTextNode("f");
+        spanVar.className = "close";
+        spanVar.innerText = txt;
+        crossList[i].appendChild(span);
+    }
+}
+
+
+function hideListItem() {
+    // Click on a close button to hide the current list item
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
+
+
